4cb7d5fee207bd2d8649df128bbb26a28aa495ec
Ytanta <121986594+Ytanta@users.noreply.github.com>
2023-10-27 07:47:58 +0300

    Update Vacanc.java

--- lesson7/observer/Vacanc.java
+++ lesson7/observer/Vacanc.java
@@ -1,5 +1,1 @@
-package geekbrains.lesson7.src.main.java.ru.geekbrains.lesson7.observer;
 
-public interface Vacanc {
-    void tableVacancy(String name, Publisher jobAgency, int maxSalary, String vacancy);
-}
